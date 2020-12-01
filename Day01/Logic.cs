1df0f23e177958bcdd290caf2c37a32f213fd5ed
Gareth Spring <gareth.spring@gmail.com>
2020-12-01 20:10:37 +0000

    Day 1

---
+++ Day01/Logic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day01
+{
+    public class Logic
+    {
+        public int Part1()
+        {
+            var input = ReadInput();
+
+            foreach (int x in input)
+            {
+                foreach (int y in input)
+                {
+                    if ( x + y == 2020)
+                    {
+                        return x * y;
+                    }
+
+                }
+            }
+
+            return 0;
+        }
+
+        public int Part2()
+        {
+            var input = ReadInput();
+
+            foreach (int x in input)
+            {
+                foreach (int y in input)
+                {
+                    foreach (int z in input)
+                    {
+                        if (x + y + z == 2020)
+                        {
+                            return x * y * z;
+                        }
+                    }
+
+                }
+            }
+
+            return 0;
+        }
+
+        private List<int> ReadInput()
+        {
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "\\Input\\Input.txt");
+            var inputList = new List<int>();
+
+            foreach (var line in input)
+            {
+                inputList.Add(Convert.ToInt32(line));
+            }
+
+            return inputList;
+        }
+    }
+}
---
+++ Day01Tests/Day01Tests.cs
@@ -0,0 +1,32 @@
+using Day01;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Day01Tests
+{
+    [TestClass]
+    public class Day01Tests
+    {
+        [TestMethod]
+        public void Day01Test()
+        {
+            var day1 = new Logic();
+
+            var actual = day1.Part1();
+            var expected = 290784;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day02Test()
+        {
+            var day1 = new Logic();
+
+            var actual = day1.Part2();
+            var expected = 177337980;
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
