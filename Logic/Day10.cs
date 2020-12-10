073756ac5486d1efd8b98c8468533f92336bbd97
Gareth Spring <gareth.spring@gmail.com>
2020-12-10 08:03:16 +0000

    Day10

---
+++ Logic/Day10.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace Logic
+{
+    public class Day10
+    {
+        private List<int> jInput;
+
+        public int Part1()
+        {
+            ReadInput();
+
+            int diff1 = 0;
+            int diff3 = 0;
+
+            for (int i = 1; i < jInput.Count; i++)
+            {
+                int diff = (jInput[i] - jInput[i - 1]);
+                if (diff == 1)
+                {
+                    diff1++;
+                }
+
+                if (diff == 3)
+                {
+                    diff3++;
+                }
+            }
+
+            return diff1 * diff3;
+        }
+
+        public long Part2()
+        {
+            ReadInput();   
+
+            var result = Part2Calc();
+
+            return result;
+        }
+
+        /// <summary>
+        /// TIL Memoization
+        /// </summary>
+        private long Part2Calc()
+        {
+            int maxDiff = 3;
+            var resultCache = new Dictionary<int, long>();
+            resultCache[jInput.Count - 1] = 1;
+
+            for (int a = jInput.Count - 2; a >= 0; a--)
+            {
+                long validCount = 0;
+
+                for (int b = a + 1; b < jInput.Count && jInput[b] - jInput[a] <= maxDiff; b++)
+                {
+                    validCount += resultCache[b];
+                }
+
+                resultCache[a] = validCount;
+            }
+
+            return resultCache[0];
+        }
+
+        private void ReadInput()
+        {
+            var input = File.ReadAllLines(Environment.CurrentDirectory + "/Input/Input10.txt");
+            jInput = new List<int>();
+
+            foreach (var line in input)
+            {
+                jInput.Add(int.Parse(line));
+            }
+
+            jInput.Sort();
+            jInput.Insert(0, 0); // Socket
+            jInput.Add(jInput.Last() + 3); // Device Adapter
+        }
+    }
+
+    public static class ArrayExtensions
+    {
+        public static void Fill<T>(this T[] originalArray, T with)
+        {
+            for (int i = 0; i < originalArray.Length; i++)
+            {
+                originalArray[i] = with;
+            }
+        }
+    }
+}
---
+++ Tests/Day10Tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Logic;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        [TestMethod]
+        public void Day10Part1Test()
+        {
+            var day10 = new Day10();
+
+            var actual = day10.Part1();
+            var expected = 1820;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day10Part2Test()
+        {
+            var day10 = new Day10();
+
+            var actual = day10.Part2();
+            var expected = 3454189699072;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
