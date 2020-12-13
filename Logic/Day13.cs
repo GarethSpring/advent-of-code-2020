4f9337432625cf1ac979cf576b7187ad440a381f
Gareth Spring <gareth.spring@gmail.com>
2020-12-13 13:02:08 +0000

    Day 13

---
+++ Logic/Day13.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic
+{
+
+    public class Day13
+    {
+        private List<string> busIds;
+        private Dictionary<int, int> earliestDepartures = new Dictionary<int, int>();
+
+        public int Part1(int earliestTimestamp, string input)
+        {
+            busIds = input.Split(',').ToList().Where(x => x.Trim() != "x").ToList();
+
+            foreach (string bus in busIds)
+            {
+                var id = int.Parse(bus);
+
+                var t = earliestTimestamp;
+
+                while (t % id != 0)
+                {
+                    t++;
+                }
+
+                earliestDepartures.Add(id, t);
+            }
+
+            var firstDeparture = earliestDepartures.Min(kvp => kvp.Value);
+            var firstDepartureBus = earliestDepartures.Aggregate((l, r) => l.Value < r.Value ? l : r).Key;
+
+            var waitTime = firstDeparture - earliestTimestamp;
+
+            return waitTime * firstDepartureBus;
+        }
+
+        /// <summary>
+        /// Optimus Prime
+        /// </summary>
+        public long Part2(string input)
+        {
+            busIds = input.Split(',').ToList().ToList();
+
+            long minTimeStamp = 0;
+            var step = long.Parse(busIds[0]);
+
+            foreach (var bus in busIds.Where(b => b.Trim() != "x").Skip(1))
+            {
+                var nextTime = int.Parse(bus);
+                while (true)
+                {
+                    minTimeStamp += step;
+
+                    // Skip gap of all primes so far and check if it matches criteria (mod zero)
+                    if ((minTimeStamp + busIds.IndexOf(bus)) % nextTime == 0)
+                    {
+                        step *= nextTime;
+                        break;
+                    }
+                }
+                
+            }
+
+            return minTimeStamp;
+        }
+    }
+}
---
+++ Tests/Day13Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day13Tests
+    {
+        [TestMethod]
+        public void Day13Part1TestExample()
+        {
+            var day13 = new Day13();
+
+            var actual = day13.Part1(939, "7, 13, x, x, 59, x, 31, 19");
+            var expected = 295;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day13Part1TestActual()
+        {
+            var day13 = new Day13();
+
+            var actual = day13.Part1(1000186, "17, x, x, x, x, x, x, x, x, x, x, 37, x, x, x, x, x, 907, x, x, x, x, x, x, x, x, x, x, x, 19, x, x, x, x, x, x, x, x, x, x, 23, x, x, x, x, x, 29, x, 653, x, x, x, x, x, x, x, x, x, 41, x, x, 13");
+            var expected = 104;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day13Part2TestExample()
+        {
+            var day13 = new Day13();
+
+            var actual = day13.Part2("7, 13, x, x, 59, x, 31, 19");
+            var expected = 1068781;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day13Part2Actual()
+        {
+            var day13 = new Day13();
+
+            var actual = day13.Part2("17, x, x, x, x, x, x, x, x, x, x, 37, x, x, x, x, x, 907, x, x, x, x, x, x, x, x, x, x, x, 19, x, x, x, x, x, x, x, x, x, x, 23, x, x, x, x, x, 29, x, 653, x, x, x, x, x, x, x, x, x, 41, x, x, 13");
+            var expected = 842186186521918;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
