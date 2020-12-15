5e104ba7d1238ab29d11a3dce5252e24dcaacb33
Gareth Spring <gareth.spring@gmail.com>
2020-12-15 11:57:02 +0000

    Day 15

---
+++ Logic/Day15.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic
+{
+    public class Day15
+    {
+        private List<string> startingNumbers;
+        private List<string> spoken = new List<string>();
+        private Dictionary<string, int> lasts = new Dictionary<string, int>();
+
+        public string Part1(string input, int haltOn)
+        {
+            startingNumbers = input.Split(',').ToList();
+
+            var last = PlaySlow(haltOn);
+
+            return last;
+        }
+
+        public string Part2(string input, int haltOn)
+        {
+            startingNumbers = input.Split(',').ToList();
+
+            var last = PlayFast(haltOn);
+
+            return last;
+        }
+
+        private string PlaySlow(int haltOn)
+        {
+            for (int j = 0; j < startingNumbers.Count; j++)
+            {
+                spoken.Add(startingNumbers[j]);
+            }
+        
+            for (int i = startingNumbers.Count; i < haltOn; i++)
+            {
+                var last = spoken[i-1];
+                // If that was the first time the number has been spoken, the current player says 0.
+                if (spoken.Where(s => s == last).Count() == 1)
+                {
+                    spoken.Add("0"); 
+                }
+                else
+                {
+                    // the current player announces how many turns apart the number is from when it was previously spoken.
+                    var allButLast = spoken.Take(i - 1).ToList();
+
+                    var indexOfSecondLast = allButLast.LastIndexOf(last);
+
+                    var diff = spoken.Count - (indexOfSecondLast + 1);
+                    spoken.Add(diff.ToString());
+                }
+            }
+
+            return spoken.Last();
+        }
+
+        private string PlayFast(int haltOn)
+        {
+            string last = "0";
+
+            for (int j = 0; j < startingNumbers.Count; j++)
+            {
+                lasts.Add(startingNumbers[j], j + 1);
+            }
+
+            for (int i = startingNumbers.Count + 1; i < haltOn; i++)
+            {
+                var next = lasts.ContainsKey(last) ? i - lasts[last] : 0;
+                lasts[last] = i;
+                last = next.ToString();
+            }
+
+            return last.ToString();
+        }
+    }
+}
---
+++ Tests/Day15Tests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day15Tests
+    {
+        [TestMethod]
+        public void Day15Part1TestExample1()
+        {
+            var day15 = new Day15();
+
+            var actual = day15.Part1("0,3,6", 10);
+            var expected = "0";
+
+            Assert.AreEqual(expected, actual); 
+        }
+
+        [TestMethod]
+        public void Day15Part1TestExample2()
+        {
+            var day15 = new Day15();
+
+            var actual = day15.Part2("1,3,2", 2020);
+            var expected = "1";
+
+            Assert.AreEqual(expected, actual); 
+        }
+
+        [TestMethod]
+        public void Day15Part1()
+        {
+            var day15 = new Day15();
+
+            var actual = day15.Part1("13,16,0,12,15,1", 2020);
+            var expected = "319";
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day15Part2()
+        {
+            var day15 = new Day15();
+
+            var actual = day15.Part2("13,16,0,12,15,1", 30000000);
+            var expected = "2424";
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
