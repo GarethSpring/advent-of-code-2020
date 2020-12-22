1a4499b2fe0e015fb3139bd5ff557ce224a241b1
Gareth Spring <gareth.spring@gmail.com>
2020-12-22 15:21:58 +0000

    Day 22

---
+++ Logic/Day22.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Logic
+{
+    public class Day22
+    {
+        List<int> p1;
+        List<int> p2;
+       
+        public long Part1(List<int> p1, List<int> p2)
+        {
+            this.p1 = p1;
+            this.p2 = p2;
+
+            return Play1();
+        }
+
+        public long Part2(List<int> p1, List<int> p2)
+        {
+            int winner = 0;
+            return Recurse2(p1, p2, ref winner);;
+        }
+
+        private long Recurse2(List<int> lp1, List<int> lp2, ref int winner)
+        {
+            List<List<int>> prevP1 = new List<List<int>>();
+            List<List<int>> prevP2 = new List<List<int>>();
+
+            while (lp1.Count != 0 && lp2.Count != 0)
+            {
+
+                for (int i = 0; i < prevP1.Count; i++)
+                {
+                    if (Enumerable.SequenceEqual(prevP1[i], lp1) && Enumerable.SequenceEqual(prevP2[i], lp2))
+                    {
+                        // Player 1 wins
+                        winner = 1;
+                        return CalcScore(lp1);
+                    }
+                }
+
+                prevP1.Add(lp1.ToList());
+                prevP2.Add(lp2.ToList());
+
+                bool p1ShouldRecurse = lp1[0] <= lp1.Count - 1;
+                bool p2ShouldRecurse = lp2[0] <= lp2.Count - 1;
+
+                if (p1ShouldRecurse && p2ShouldRecurse)
+                {
+                    // New game
+                    Recurse2(lp1.Skip(1).Take(lp1[0]).ToList(), lp2.Skip(1).Take(lp2[0]).ToList(), ref winner);
+
+                    if (winner == 1)
+                    {
+                        lp1.Add(lp1[0]);
+                        lp1.Add(lp2[0]);
+                        lp1.RemoveAt(0);
+
+                        lp2.RemoveAt(0);
+                    }
+                    else if (winner == 2)
+                    {
+                        lp2.Add(lp2[0]);
+                        lp2.Add(lp1[0]);
+                        lp2.RemoveAt(0);
+                        lp1.RemoveAt(0);
+                    }
+                }
+                else
+                {
+                    winner = ActionWin(lp1, lp2);
+                }
+
+            }
+
+            if (lp1.Count == 0)
+            {
+                winner = 2;
+                return CalcScore(lp2);
+            }
+
+            winner = 1;
+
+            return CalcScore(lp1);
+        }
+
+        private int ActionWin(List<int> lp1, List<int> lp2)
+        {
+            if (lp1[0] > lp2[0])
+            {
+                lp1.Add(lp1[0]);
+                lp1.Add(lp2[0]);
+                lp1.RemoveAt(0);
+                lp2.RemoveAt(0);
+                return 1;
+            }
+
+            lp2.Add(lp2[0]);
+            lp2.Add(lp1[0]);
+            lp2.RemoveAt(0);
+            lp1.RemoveAt(0);
+
+            return 2;
+        }
+
+        private long Play1()
+        {
+            while (p1.Count != 0 && p2.Count != 0)
+            {
+                if (p1[0] > p2[0])
+                {
+                    p1.Add(p1[0]);
+                    p1.Add(p2[0]);
+                    p1.RemoveAt(0);
+
+                    p2.RemoveAt(0);
+                }
+                else
+                {
+                    p2.Add(p2[0]);
+                    p2.Add(p1[0]);
+                    p2.RemoveAt(0);
+                    p1.RemoveAt(0);
+                }
+
+            }
+
+            return p1.Count == 0 ? CalcScore(p2) : CalcScore(p1);
+        }
+
+        private long CalcScore(List<int> cards)
+        {
+            long score = 0;
+            int multiplier = 1;
+
+            for (int i = cards.Count - 1; i >= 0; i--)
+            {
+                score += cards[i] * multiplier;
+                multiplier++;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ Tests/Day22Tests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day22Tests
+    {
+        private List<int> p1 = new List<int>() { 26, 16, 33, 8, 5, 46, 12, 47, 39, 27, 50, 10, 34, 20, 23, 11, 43, 14, 18, 1, 48, 28, 31, 38, 41 };
+        private List<int> p2 = new List<int>() { 45, 7, 9, 4, 15, 19, 49, 3, 36, 25, 24, 2, 21, 37, 35, 44, 29, 13, 32, 22, 17, 30, 42, 40, 6 };
+
+        [TestMethod]
+        public void Day22Part1Test()
+        {
+            var day22 = new Day22();
+
+            var actual = day22.Part1(p1, p2);
+            var expected = 31957;   
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day22Part1ExampleTest()
+        {
+            var day22 = new Day22();
+
+            var actual = day22.Part1(new List<int>() { 9, 2, 6, 3, 1 }, new List<int>() { 5, 8, 4, 7, 10 });
+            var expected = 306; 
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day22Part2ExampleTest()
+        {
+            var day22 = new Day22();
+
+            var actual = day22.Part2(new List<int>() { 9, 2, 6, 3, 1 }, new List<int>() { 5, 8, 4, 7, 10 });
+            var expected = 291;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day22Part2Test()
+        {
+            var day22 = new Day22();
+
+            var actual = day22.Part2(p1, p2);
+            var expected = 33212;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
