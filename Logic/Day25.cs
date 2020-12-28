6b28c36754839d1e87289d4496ea84e16771d830
Gareth Spring <gareth.spring@gmail.com>
2020-12-28 11:55:55 +0000

    Day 25

---
+++ Logic/Day25.cs
@@ -0,0 +1,49 @@
+﻿namespace Logic
+{
+    public class Day25
+    {
+        private int subjectNum = 7;
+
+        public long Part1(int pubKey1, int pubKey2)
+        {
+            var loopSize1 = GetLoopSize(pubKey1);
+            var loopSize2 = GetLoopSize(pubKey2);
+
+            var enc1 = GetEncryptionKey(pubKey1, loopSize2);
+            var enc2 = GetEncryptionKey(pubKey2, loopSize1);
+
+            return enc1;
+        }
+
+        private int GetLoopSize(long key)
+        {
+            long value = 1;
+            int loopSize = 0;
+
+            while(value != key)
+            {
+                loopSize++;
+                value *= subjectNum;
+                var remainder = value % 20201227;
+                value = remainder;
+            }
+
+            // return loopsize
+            return loopSize;
+        }
+
+        private long GetEncryptionKey(long key, int loopSize)
+        {
+            long value = 1;
+           
+            for (int i = 1; i <= loopSize; i++)
+            {
+                value *= key;
+                var remainder = value % 20201227;
+                value = remainder;
+            }
+
+            return value;
+        }
+    }
+}
---
+++ Tests/Day25Tests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day25Tests
+    {
+        [TestMethod]
+        public void Day25Part1TestExample()
+        {
+            var day25 = new Day25();
+
+            var actual = day25.Part1(5764801, 17807724);
+            var expected = 14897079;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Day25Part1Test()
+        {
+            var day25 = new Day25();
+
+            var actual = day25.Part1(10441485, 1004920);
+            var expected = 17032383; 
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
